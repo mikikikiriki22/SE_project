96c7c4442018ad0d398ea74178d2dce6580f08e4
mirzacesir <mirza.cesir@stu.ibu.edu.ba>
2025-06-16 00:17:17 +0200

    url change

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 const Constants = {
-    PROJECT_BASE_URL: "https://starfish-app-3sxaq.ondigitalocean.app/backend/",
+    PROJECT_BASE_URL: "https://starfish-app-3sxaq.ondigitalocean.app/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
 }; 
